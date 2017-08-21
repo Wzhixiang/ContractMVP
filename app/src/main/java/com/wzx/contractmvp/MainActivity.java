e3ea7282e185607511dcf79a9188dc56ac37d7d0
wangzhixiang <825832326@qq.com>
2017-08-21 19:46:51 +0800

    添加头部注解

--- app/src/main/java/com/wzx/contractmvp/MainActivity.java
+++ app/src/main/java/com/wzx/contractmvp/MainActivity.java
@@ -3,6 +3,9 @@ package com.wzx.contractmvp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 添加头部说明
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
