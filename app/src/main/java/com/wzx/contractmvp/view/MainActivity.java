b3c79af2f48cc712a39b6bd3ec708c6016ac0956
wangzhixiang <825832326@qq.com>
2017-09-10 11:06:25 +0800

    结构优化

---
+++ app/src/main/java/com/wzx/contractmvp/view/MainActivity.java
@@ -0,0 +1,58 @@
+package com.wzx.contractmvp.view;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.mvplib.BaseActivity;
+import com.wzx.contractmvp.contract.ContractMain;
+import com.wzx.contractmvp.R;
+import com.wzx.contractmvp.presenter.PresenterMain;
+
+/**
+ * 添加头部说明
+ */
+public class MainActivity extends BaseActivity<ContractMain.MView, PresenterMain> implements ContractMain.MView {
+
+    private TextView textView;
+
+    @Override
+    protected void initVariables() {
+
+    }
+
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+        textView = (TextView) findViewById(R.id.tv_change_text);
+        textView.setOnClickListener(v->mPresenter.requestText());
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected PresenterMain createPresenter() {
+        return new PresenterMain();
+    }
+
+    @Override
+    public void showProgress() {
+
+    }
+
+    @Override
+    public void hideProgress() {
+
+    }
+
+    @Override
+    public void showText(String string) {
+        textView.setText(string);
+    }
+}
---
+++ mvplib/src/main/java/com/example/mvplib/BaseView.java
@@ -0,0 +1,13 @@
+package com.example.mvplib;
+
+/**
+ * 描述 TODO
+ * Created by 王治湘 on 2017/9/4.
+ * version 1.0
+ */
+
+public interface BaseView {
+    void showProgress();
+
+    void hideProgress();
+}
