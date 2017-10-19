2233f5e649cfdfb0c2b735d55969b2bf04693007
wangzhixiang <825832326@qq.com>
2017-10-19 12:16:18 +0800

    添加授权

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ContractMVP
-DropMenuView :添加指示器（indicator）
-    当DropMenuView中的tab展开时，在tab标题下添加指示划线
-    实现方式采用自定义IndicatorDrawable，在tab状态变化时，加载不同IndicatorDrawable来实现
+##DropMenuView :添加指示器（indicator）
+        当DropMenuView中的tab展开时，在tab标题下添加指示划线
+        实现方式采用自定义IndicatorDrawable，在tab状态变化时，加载不同IndicatorDrawable来实现
--- app/src/main/java/com/wzx/contractmvp/view/BaseActivity.java
+++ app/src/main/java/com/wzx/contractmvp/view/BaseActivity.java
@@ -1,8 +1,15 @@
 package com.wzx.contractmvp.view;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
+import com.wzx.contractmvp.R;
 import com.wzx.contractmvp.presenter.BasePresenter;
 
 
@@ -14,6 +21,7 @@ import com.wzx.contractmvp.presenter.BasePresenter;
 
 @SuppressWarnings("unchecked")
 public abstract class BaseActivity<V, P extends BasePresenter<V>> extends AppCompatActivity {
+
     protected P mPresenter;
 
     @Override
@@ -43,7 +51,57 @@ public abstract class BaseActivity<V, P extends BasePresenter<V>> extends AppCom
     protected void onDestroy() {
         super.onDestroy();
         mPresenter.detachView();
+
+        //判断隐藏软键盘是否弹出
+        if (getWindow().getAttributes().softInputMode == WindowManager.LayoutParams.SOFT_INPUT_STATE_UNSPECIFIED) {
+            //隐藏软键盘
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        }
     }
+
+    /**
+     * Requests given permission.
+     * If the permission has been denied previously, a Dialog will prompt the user to grant the
+     * permission, otherwise it is requested directly.
+     */
+    protected void requestPermission(final String permission, String rationale, final int requestCode) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+            showAlertDialog("请求授权", rationale,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(BaseActivity.this,
+                                    new String[]{permission}, requestCode);
+                        }
+                    }, "确定", null, "拒绝");
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
+        }
+    }
+
+    /**
+     * This method shows dialog with given title & message.
+     * Also there is an option to pass onClickListener for positive & negative button.
+     *
+     * @param title                         - dialog title
+     * @param message                       - dialog message
+     * @param onPositiveButtonClickListener - listener for positive button
+     * @param positiveText                  - positive button text
+     * @param onNegativeButtonClickListener - listener for negative button
+     * @param negativeText                  - negative button text
+     */
+    protected void showAlertDialog(@Nullable String title, @Nullable String message,
+                                   @Nullable DialogInterface.OnClickListener onPositiveButtonClickListener,
+                                   @NonNull String positiveText,
+                                   @Nullable DialogInterface.OnClickListener onNegativeButtonClickListener,
+                                   @NonNull String negativeText) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton(positiveText, onPositiveButtonClickListener);
+        builder.setNegativeButton(negativeText, onNegativeButtonClickListener);
+    }
+
 
     protected abstract int getLayoutID();
 
